0eb5bd27ddee80fbaa634ce24263dd22f780cadf
Rex Chiu <rex@iotum.com>
2020-10-13 14:46:49 -0400

    add in missing imports

--- src/android/BackgroundModeExt.java
+++ src/android/BackgroundModeExt.java
@@ -30,6 +30,8 @@ import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
